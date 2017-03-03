c5fafcbb8243bd27520bcc710627fe444669f901
Magnus Holm <mail@magnusholm.com>
2017-03-03 17:21:28 +0000

    Refactored the code and implemented feedback messages

--- directory.rb
+++ directory.rb
@@ -2,47 +2,46 @@
 
 #Asks user to input details about student such as name and cohort. Saves inputs as hashes in @students array.
 def input_students
-  puts "Please enter the name, country of origin, hobby and height of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the following info about the students. To finish, hit return twice.\n"
 
   valid_months = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
 
   puts "Name:"
-  name = STDIN.gets.delete("\n")
+  name = STDIN.gets.chomp
 
   while !name.empty? do
 
     puts "Country:"
-    country = STDIN.gets.delete("\n")
+    country = STDIN.gets.chomp
       break if country.empty?
 
     puts "Hobby:"
-    hobby = STDIN.gets.delete("\n")
+    hobby = STDIN.gets.chomp
       break if hobby.empty?
 
     puts "Height:"
-    height = STDIN.gets.delete("\n")
+    height = STDIN.gets.chomp
       break if height.empty?
 
     puts "Cohort:"
-    cohort = STDIN.gets.delete("\n").downcase.to_sym
+    cohort = STDIN.gets.chomp.downcase.to_sym
       break if cohort.empty?
         while !valid_months.include?(cohort)
           puts "Please enter a calendar month"
           cohort = STDIN.gets.delete("\n").downcase.to_sym
         end
 
-    #@students << {name: name, country: country, hobby: hobby, height: height, cohort: cohort}
     add_students_to_list(name, country, hobby, height, cohort)
+    puts "#{name} was succesfully added to the #{cohort.capitalize} cohort."
 
     if @students.count > 1
-      puts "Now we have #{@students.count} students"
+      puts "Now we have #{@students.count} students\n"
     else
-      puts "Now we have #{@students.count} student"
+      puts "Now we have #{@students.count} student\n"
     end
 
     puts "Name:"
-    name = STDIN.gets.delete("\n")
+    name = STDIN.gets.chomp
       break if name.empty?
   end
 
@@ -51,9 +50,9 @@ end
 
 #Prints header only if there are students to list.
 def print_header
-  if @students.length > 0
-    puts "The students of Villains Academy".center(70)
-    puts "------------".center(70)
+  if @students.count > 0
+    puts "The students of Villains Academy:\n"
+    puts "------------\n"
   end
 end
 
@@ -62,7 +61,7 @@ def print_student_list
   if @students.length > 0
     cohorts = @students.map do |student| student[:cohort] end
       cohorts.uniq.each do |x|
-        puts "#{x.capitalize} Cohort:"
+        puts "#{x.capitalize} cohort:"
         @students.each do |student|
         if student[:cohort] == x
           puts "- Name: #{student[:name]}, Country of Origin: #{student[:country]}, Hobby: #{student[:hobby]}, Height: #{student[:height]}"
@@ -83,7 +82,6 @@ end
 
 #Prints options for user inputs.
 def print_menu
-  puts " "
   puts "What would you like to do?\n"
   puts "1. Input the students"
   puts "2. Show the students"
@@ -133,6 +131,7 @@ def save_students
     csv_line = student_data.join(",")
     file.puts csv_line
   end
+  puts "The changes have been saved.\n"
   file.close
 end
 
@@ -147,7 +146,7 @@ def load_students(filename = "students.csv")
 end
 
 def try_load_students
-  filename = ARGV.first #first argument from the command line
+  filename = ARGV.first
   if filename.nil?
     load_students()
   elsif File.exists?(filename)
@@ -161,6 +160,7 @@ end
 
 def add_students_to_list(name, country, hobby, height, cohort)
   @students << {name: name, country: country, hobby: hobby, height: height, cohort: cohort.to_sym}
+
 end
 
 try_load_students
