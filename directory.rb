8302d8798a80bd5f5a8abae5ea450f5d3ab2a262
Magnus Holm <mail@magnusholm.com>
2017-03-03 10:43:22 +0000

    made student array instance variable and refactored interactive menu

--- directory.rb
+++ directory.rb
@@ -1,8 +1,9 @@
+@students = [] #accesible to all methods.
+
 def input_students
   puts "Please enter the name, country of origin, hobby and height of the students"
   puts "To finish, just hit return twice"
 
-  students = []
   valid_months = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
 
   puts "Name:"
@@ -30,12 +31,12 @@ def input_students
           cohort = gets.delete("\n").downcase.to_sym
         end
 
-    students << {name: name, country: country, hobby: hobby, height: height, cohort: cohort}
+    @students << {name: name, country: country, hobby: hobby, height: height, cohort: cohort}
 
-    if students.count > 1
-      puts "Now we have #{students.count} students"
+    if @students.count > 1
+      puts "Now we have #{@students.count} students"
     else
-      puts "Now we have #{students.count} student"
+      puts "Now we have #{@students.count} student"
     end
 
     puts "Name:"
@@ -43,22 +44,22 @@ def input_students
       break if name.empty?
   end
 
-  students
+  @students
 end
 
-def print_header(students)
-  if students.length > 0
+def print_header
+  if @students.length > 0
     puts "The students of Villains Academy".center(70)
     puts "------------".center(70)
   end
 end
 
-def print_by_cohort(students)
-  if students.length > 0
-    cohorts = students.map do |student| student[:cohort] end
+def print_student_list
+  if @students.length > 0
+    cohorts = @students.map do |student| student[:cohort] end
       cohorts.uniq.each do |x|
         puts "#{x.capitalize} Cohort:"
-        students.each do |student|
+        @students.each do |student|
         if student[:cohort] == x
           puts "- Name: #{student[:name]}, Country of Origin: #{student[:country]}, Hobby: #{student[:hobby]}, Height: #{student[:height]}"
         end
@@ -69,34 +70,44 @@ def print_by_cohort(students)
   end
 end
 
-def print_footer(names)
-  if names.length > 0
-    puts "Overall, we have #{names.count} students."
+def print_footer
+  if @students.length > 0
+    puts "Overall, we have #{@students.count} students."
   end
 end
 
+def print_menu
+  puts " "
+  puts "What would you like to do?\n"
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
+
+
 def interactive_menu
-  students = []
   loop do
-
-    puts "1. Inputs the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    selection = gets.chomp
-
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header(students)
-        print_by_cohort(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "I don't know what you mean, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
